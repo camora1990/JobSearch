817eec037a22d79f2faee663ec92b0c49b9cdbf7
camora1990 <moorales.camilo@gmail.com>
2022-05-07 14:55:42 -0500

    Instalcion de dependencias, configuracion de variabes de entorno

---
+++ backend/src/config/config.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+
+const config={
+    port: process.env.PORT,
+    connection_string: process.env.CONNECTION_STRING,
+    private_jwt:process.env.PRIVATE_JWT
+}
+
+export default config
