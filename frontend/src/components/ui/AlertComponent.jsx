b9972a9138351e2585dda40b9c54851f22cebbfc
WillyEsco <willyescobar@yandex.com>
2022-05-18 19:56:25 -0300

    mensaje logo brand

--- frontend/src/components/ui/AlertComponent.jsx
+++ frontend/src/components/ui/AlertComponent.jsx
@@ -8,16 +8,15 @@ const AlertComponent = ({ title, description, footer }) => {
       <div className="alert bg-dark" role="alert">
         <h4 className="alert-heading">Well done!</h4>
         <p>
-          Aww yeah, you successfully read this important alert message. This
-          example text is going to run a bit longer so that you can see how
-          spacing within an alert works with this kind of content.
+                     "BIENVENIDO A JOBSEARCH!!!      
         </p>
         <hr />
         <p className="mb-0">
-          Whenever you need to, be sure to use margin utilities to keep things
-          nice and tidy.
+        En este panel personal encontrarás todas tus ofertas.
         </p>
-      </div>
+         
+        </div>
+        
     </>
   );
 };
--- frontend/src/components/ui/NavBar.jsx
+++ frontend/src/components/ui/NavBar.jsx
@@ -20,7 +20,7 @@ export const NavBar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         
+        
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <NavLink
