0ff70f7222becf0c98a3ed7ae6235246940d2350
WillyEsco <willyescobar@yandex.com>
2022-05-21 22:34:43 -0300

    video leyend

--- frontend/src/pages/HomeScreen.jsx
+++ frontend/src/pages/HomeScreen.jsx
@@ -5,11 +5,21 @@ const HomeScreen = () => {
     
     <div className="row d-flex justify-content-center mt-5">
         <div className="col-12 col-lg-6">
-            <video autoPlay src="/assets/videos/jobSearch.mp4"></video>
+            <video loop src="/assets/videos/jobSearch.mp4" controls type="video/mp4"></video>
+         
         </div>
+        <center>
+        <div>      
+          <br></br>
+          <h3>Hello welcome!!!, <br></br>
+             This video describes in a few steps what is the process of <br></br>  searching and applying for job offers through JobSearch APP. 
+             <br></br> <br></br>You will have news before you eat a donut !!!
+             <br></br> Very good luck!!!! </h3>  
+             </div>
+             </center>
     </div>
 
   )
 }
-
+ 
 export default HomeScreen
